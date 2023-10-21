a85738595a680e479adccbea36874167a69d779c
splitcentre <raihanalfianpratama@gmail.com>
2023-10-21 22:31:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/login_menu/LoginFragment.kt
@@ -0,0 +1,55 @@
+package com.example.login_menu
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.login_menu.databinding.FragmentLoginBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var binding: FragmentLoginBinding // Use the correct binding class
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            val username = binding.usernameEditText.text.toString()
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            intent.putExtra(EXTRA_NAME, username)
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LoginFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
