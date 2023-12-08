b104cf470c668689aa90d26691fac025401470cf
Raihan "Splitcentre" Alfian Pratama <100069684+splitcentre@users.noreply.github.com>
2023-12-08 14:46:40 +0700

    some tweak to LoginFragment and add activity_admin.xml

--- app/src/main/java/com/example/login_menu/MainActivity.kt
+++ app/src/main/java/com/example/login_menu/MainActivity.kt
@@ -5,22 +5,32 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.login_menu.databinding.ActivityMainBinding
 
-class
-
-
-
-MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        with(binding){
-            viewPager.adapter = TabAdapter(supportFragmentManager)
-            tabLayout.setupWithViewPager(viewPager)
+        replaceFragment(LoginFragment())
+
+        binding.botnav.setOnItemReselectedListener{
+            when(it.itemId){
+                R.id.loginmenu -> replaceFragment(LoginFragment())
+                R.id.registermenu -> replaceFragment(RegisterFragment())
+
+                else -> {}
+            }
         }
+        true
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
