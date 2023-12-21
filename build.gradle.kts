80d5330b00f98fad440259cf9ab075552c023df1
Raihan "Splitcentre" Alfian Pratama <100069684+splitcentre@users.noreply.github.com>
2023-12-21 10:05:15 +0700

    done?

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
         classpath("com.google.gms:google-services:4.4.0")
     }
 }
+//no error
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.0" apply false
